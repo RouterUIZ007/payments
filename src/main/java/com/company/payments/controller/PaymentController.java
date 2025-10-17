1d79af0e5dce1c88d78d5878125dadd0bea29423
RouterUIZ007 <router.uiz.360@gmail.com>
2025-10-17 02:51:33 -0600

    fix(payment): fix controller createPayment

--- src/main/java/com/company/payments/controller/PaymentController.java
+++ src/main/java/com/company/payments/controller/PaymentController.java
@@ -1,29 +1,15 @@
 package com.company.payments.controller;
 
-import com.company.payments.exception.ConflictException;
-import com.company.payments.exception.ResourceNotFoundException;
-import com.company.payments.exception.UnprocessableEntityException;
-import com.company.payments.model.Payment;
 import com.company.payments.model.request.PaymentRequest;
-import com.company.payments.model.response.*;
+import com.company.payments.model.request.SearchPaymentRequest;
 import com.company.payments.service.PaymentService;
-import org.springframework.data.domain.Page;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -32,25 +18,6 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
-
-    @PostMapping("/payment")
-    public ResponseEntity<?> createPayment(
-            @Valid
-            @RequestBody
-            PaymentRequest request,
-            BindingResult bindingResult
-    ) {
-        if (bindingResult.hasErrors()) {
-
-            List<String> errorsResult = bindingResult.getAllErrors().stream()
-                    .map(ObjectError::getDefaultMessage)
-                    .collect(Collectors.toList());
-            String errors = String.join(", ", errorsResult);
-
-            NoCreatePaymentResponse payment = NoCreatePaymentResponse.builder()
-                    .status("-1")
-                    .message("Payment not created: " + errors)
-                    .build();
     @PostMapping("/payment")
     public ResponseEntity<?> createPayment(@Valid @RequestBody PaymentRequest request, BindingResult bindingResult) {
         var payment = paymentService.savePayment(request, bindingResult);
